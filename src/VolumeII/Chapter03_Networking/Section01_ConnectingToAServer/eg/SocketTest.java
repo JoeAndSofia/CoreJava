3058561cab8bf45de4f106a40470b3fac570cbed
BearJoeWYL <wylsniper@163.com>
2017-04-25 09:30:49 +0800

    mk eg folder for examples

--- src/VolumeII/Chapter03_Networking/Section01_ConnectingToAServer/eg/SocketTest.java
+++ src/VolumeII/Chapter03_Networking/Section01_ConnectingToAServer/eg/SocketTest.java
@@ -1,4 +1,4 @@
-package VolumeII.Chapter03_Networking.Section01_ConnectingToAServer;
+package VolumeII.Chapter03_Networking.Section01_ConnectingToAServer.eg;
 
 import java.io.IOException;
 import java.io.InputStream;
